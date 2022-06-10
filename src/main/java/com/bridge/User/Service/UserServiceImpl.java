d5ba0e2e42bdbd12145efda39b6738edc77fcd1e
NoaKafka <35343777+NoaKafka@users.noreply.github.com>
2022-06-10 10:42:30 +0900

    change_DI

--- src/main/java/com/bridge/User/Service/UserServiceImpl.java
+++ src/main/java/com/bridge/User/Service/UserServiceImpl.java
@@ -3,15 +3,18 @@ package com.bridge.User.Service;
 import com.bridge.User.Data.JoinRequest;
 import com.bridge.User.Data.UserEntity;
 import com.bridge.User.Repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-
+@Component
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService{
 
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+
     @Transactional
     @Override
     public void joinUser(JoinRequest joinRequest) {
