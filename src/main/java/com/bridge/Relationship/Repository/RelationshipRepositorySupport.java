a24a63f6b0b44aeb2c885b3bc8e0544a3f09c02d
NoaKafka <35343777+NoaKafka@users.noreply.github.com>
2022-06-20 21:55:27 +0900

    querydsl part1
    구현 미완성

---
+++ src/main/java/com/bridge/Relationship/Repository/RelationshipRepositorySupport.java
@@ -0,0 +1,29 @@
+package com.bridge.Relationship.Repository;
+
+import com.bridge.Relationship.Data.QRelationshipEntity;
+import com.bridge.Relationship.Data.RelationshipEntity;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+import static com.bridge.Relationship.Data.QRelationshipEntity.relationshipEntity;
+
+@Repository
+public class RelationshipRepositorySupport extends QuerydslRepositorySupport {
+
+    private final JPAQueryFactory queryFactory;
+
+    public RelationshipRepositorySupport(JPAQueryFactory queryFactory) {
+        super(RelationshipEntity.class);
+        this.queryFactory = queryFactory;
+    }
+
+    public List<RelationshipEntity> findByUserName(String userName){
+        return queryFactory
+                .selectFrom(relationshipEntity)
+                .where(relationshipEntity.userName.eq(userName))
+                .fetch();
+    }
+}
